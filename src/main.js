9cf04280504e4c5ab95f0ac2fe99615f61c7d8c5
alexandra-catherine <acl21@colostate.edu>
2020-11-28 13:04:24 -0700

    13.2 11/28 axios, signup

--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
+import axios from 'axios'
 
 import router from './routes.js'
+
+axios.defaults.baseURL = "your base website";
+
 new Vue({
   el: '#app',
   render: h => h(App)
